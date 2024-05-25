c7915b32ce27d4886bea41620d601f8d1ad73b5a
Georgii <149330707+sitnikovGOSHA@users.noreply.github.com>
2024-05-25 15:20:53 +0300

    Финальное ревью

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ const allowedCors = [
   "https://students-projects.ru",
   "http://localhost:3000",
   "http://localhost:3001",
-  "https://goshapindiefrontyp.nomoredomainswork.ru"
+  "https://goshapindiefrontyp.nomoredomainswork.ru/",
 ];
 
 function cors(req, res, next) {
